cc062d35db27279fd98f6c49492d57323a379274
Eleonora Shantsila <eleonora.shantsila@gmail.com>
2025-09-09 20:46:26 +0100

    comment out chat tab and update styles

--- src/components/client-workspace/ClientWorkspace.tsx
+++ src/components/client-workspace/ClientWorkspace.tsx
@@ -1,12 +1,11 @@
 import { AnalysisResults } from '../analysis/AnalysisResults';
-import { ChatInterface } from '../chat-interface/ChatInterface';
 import { ClientDetails } from '../client-details';
 import { DocumentManager } from '../document-manages/DocumentManager';
 
 
 export const ClientWorkspace = () => {
   return (
-    <div className="flex flex-1 overflow-hidden grid grid-cols-3 gap-6 p-6">
+    <div className="flex flex-1 overflow-hidden grid grid-cols-2 gap-6 p-6">
       {/* Left Panel - Documents and Client Info */}
       <div className="flex-1 flex-col overflow-y-auto overflow-x-hidden">
         {/* flex flex-1 flex-row grid grid-row-2 gap-6 */}
@@ -25,9 +24,9 @@ export const ClientWorkspace = () => {
       </div>
 
       {/* Right Panel - Chat Interface */}
-      <div className="flex-1 overflow-y-auto">
+      {/* <div className="flex-1 overflow-y-auto">
         <ChatInterface />
-      </div>
+      </div> */}
     </div>
   );
 };
