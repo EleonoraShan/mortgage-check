7020701374ffedb9f04c66f7be92636d339c6c4b
Eleonora Shantsila <eleonora.shantsila@gmail.com>
2025-09-11 17:57:17 +0100

    fix splash screen

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,7 +1,7 @@
 
 export const SplashScreen = () => {
   return (
-    <div className="h-screen w-screen bg-background flex items-center justify-center p-6 absolute bottom-0 left-0 right-0 top-0 z-1000">
+    <div className="fixed inset-0 z-[1000] bg-background flex items-center justify-center p-6">
       <img
         src="/wordmark.png"
         alt="Lendomus"
