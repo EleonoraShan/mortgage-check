52c0f5641051e58c69b42e483a2a656f4884aa22
Eleonora Shantsila <eleonora.shantsila@gmail.com>
2025-09-11 17:54:30 +0100

    only show welcome once

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,7 +1,7 @@
 
 export const SplashScreen = () => {
   return (
-    <div className="min-h-screen bg-background flex items-center justify-center p-6 absolute bottom-0 left-0 right-0 top-0">
+    <div className="h-screen w-screen bg-background flex items-center justify-center p-6 absolute bottom-0 left-0 right-0 top-0 z-1000">
       <img
         src="/wordmark.png"
         alt="Lendomus"
